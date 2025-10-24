46aa2e5ec37fb790169013023d06255ee2c9710d
GitHub Action <action@github.com>
2025-10-24 18:02:27 +0000

    Initialize template repository proper
    Set package name to com.github.rohitnikamm.bedwarsprov1
    Set project name to bedwars-pro-v1
    Set modid to bedwarsprov

--- src/main/java/com/github/rohitnikamm/bedwarsprov1/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/rohitnikamm/bedwarsprov1/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.rohitnikamm.bedwarsprov1.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/rohitnikamm/bedwarsprov1/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/rohitnikamm/bedwarsprov1/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.rohitnikamm.bedwarsprov1.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
